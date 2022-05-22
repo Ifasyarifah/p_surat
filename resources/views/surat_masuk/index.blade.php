9a33715ba6dd34c21b5d7441bfe6ec4cd11a1797
Ifasyarifah <ifaoneme@gmail.com>
2022-05-22 07:37:39 +0700

    menambahan crud/index

--- resources/views/surat_masuk/index.blade.php
+++ resources/views/surat_masuk/index.blade.php
@@ -80,25 +80,28 @@
   </div>
 </div> 
 <!-- script -->
-<script src="{{ asset('') }}assets/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="{{ asset('') }}assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="{{ asset('') }}assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="{{ asset('') }}assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="{{ asset('') }}assets/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="{{ asset('') }}assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="{{ asset('') }}assets/plugins/jszip/jszip.min.js"></script>
-<script src="{{ asset('') }}assets/plugins/pdfmake/pdfmake.min.js"></script>
-<script src="{{ asset('') }}assets/plugins/pdfmake/vfs_fonts.js"></script>
-<script src="{{ asset('') }}assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="{{ asset('') }}assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="{{ asset('') }}assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+@section('javascript')
 <script>
-  $(function () {
-    $("#Suratmasuk").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,
-      "buttons": ["print"]
-    }).buttons().container().appendTo('#Suratmasuk_wrapper .col-md-6:eq(0)');
-  });
+    $('#kelas-table').DataTable({
+        processing: true,
+        serverside: true,
+        ajax: {
+            url: "{{ route('surat_masuk.index') }}",
+            type: 'GET',
+        },
+        "responsive": true,
+        columns: [{
+                data: 'DT_RowIndex',
+            },
+            {
+                data: 'surat_masuk',
+            },
+        ]
+    });
+
+    
+</script>
+@endsection
 </script>             
 @endsection
   
